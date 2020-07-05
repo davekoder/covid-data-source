873769f6c3935f1ff28794aa311cbf03585fc243
Davekoder <davekoder@gmail.com>
2020-07-05 18:01:58 +0800

    Updates

---
+++ precache-manifest.5350571e92b69fdb2885e253f84404a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345dc9d586bd727fe67fb1b7050c6e4f",
+    "url": "/covid-data-source/index.html"
+  },
+  {
+    "revision": "1ff99c63d4206f007220",
+    "url": "/covid-data-source/static/css/main.4577cc5b.chunk.css"
+  },
+  {
+    "revision": "e0e92c885bf81d663517",
+    "url": "/covid-data-source/static/js/2.88487b2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-data-source/static/js/2.88487b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff99c63d4206f007220",
+    "url": "/covid-data-source/static/js/main.573225a8.chunk.js"
+  },
+  {
+    "revision": "2a24561a9fb98c5f4bec",
+    "url": "/covid-data-source/static/js/runtime-main.eb8fbcf5.js"
+  },
+  {
+    "revision": "73a4d84936391f023e26480d311c2628",
+    "url": "/covid-data-source/static/media/background-video.73a4d849.mp4"
+  }
+]);
