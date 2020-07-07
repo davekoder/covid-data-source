cded008575e85bd466b34af82c8e879069d1fa08
Davekoder <davekoder@gmail.com>
2020-07-07 22:01:41 +0800

    Created basic media queries

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import "./screen-sizes/320.styles.scss";
+import "./screen-sizes/481.styles.scss";
+import "./screen-sizes/769.styles.scss";
+import "./screen-sizes/1025.styles.scss";
+
 ReactDOM.render(<App />, document.getElementById("root"));
